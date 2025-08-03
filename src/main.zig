d6a3c9c913ce7f58487b7f5a24b86b43a769fc99
Henry Kupty <hkupty@gmail.com>
2025-08-03 14:56:20 +0200

    docs: Add next steps for cli handling

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,14 @@
 const std = @import("std");
 const lib = @import("lift_lib");
 
+// TODO: perhaps daemonize in a liftd binary + lift to communicate w/ daemon?
+// TODO: (daemon) create a collection/mapping of projects based on names - perhaps from lift.toml + path-based
+// TODO: (daemon) Invalidate and re-process project based on file changes
+// TODO: (frontend) handle `[project:]target` argument structure
+// TODO: (frontend) handle `[./path/to/project:]target` argument structure
+// TODO: (frontend) handle multiple arguments
+// TODO: (frontend) handle verbosity
+
 pub fn main() !void {
     std.debug.print("Not ready yet.\n", .{});
 }
