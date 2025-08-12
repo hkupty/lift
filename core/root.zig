1336aaf65b7d60e06f3d6486afd0b0be24e949d1
Henry Kupty <hkupty@gmail.com>
2025-08-13 00:01:30 +0200

    test: Include utils in test

--- core/root.zig
+++ core/root.zig
@@ -2,7 +2,9 @@ const std = @import("std");
 const testing = std.testing;
 
 pub const project = @import("project.zig");
+const utils = @import("utils.zig");
 
 test "run all tests" {
     testing.refAllDecls(project);
+    testing.refAllDecls(utils);
 }
