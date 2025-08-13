d4fc13f311f5f015d2a09f77dcf0240f7e7db41e
Henry Kupty <hkupty@gmail.com>
2025-08-13 17:38:51 +0200

    feat: Recursively process dependencies

--- dependencies/main.zig
+++ dependencies/main.zig
@@ -6,8 +6,8 @@ const worker = @import("worker.zig");
 const Pool = worker.WorkerPool;
 const spec = @import("spec.zig");
 const http = @import("http.zig");
+const json = std.json;
 const pomParser = @import("pom.zig");
-
 const LocaLRepo = @import("local_repo.zig").LocalRepo;
 
 // TODO: Enqueue new dependencies based on POM results recursively;
@@ -52,99 +52,105 @@ pub fn main() !void {
         }
     }
 
-    var depsset = std.BufSet.init(allocator);
-    defer depsset.deinit();
-
     var downloadManager = try http.init(allocator);
     defer downloadManager.deinit();
 
-    // TODO: Enqueue dependencies declared in POM
+    var dependencies = std.StringArrayHashMap(spec.Asset).init(allocator);
+    defer dependencies.deinit();
 
+    // NOTE: Dependency conflict resolution might cause changes the dependency list
+
+    // TODO: Enqueue dependencies declared in POM
     for (stepConfig.data) |directive| {
         const dep = try spec.Asset.parse(allocator, directive);
         errdefer dep.deinit();
         allocator.free(directive);
+        const identifier = try dep.identifier(allocator);
+        defer allocator.free(identifier);
 
-        const identifier = dep.identifier(allocator) catch |err| {
-            std.log.err("Failed to format dependency identifier: {any}", .{err});
+        const value = try dependencies.getOrPut(identifier);
+
+        if (value.found_existing) {
+            std.log.info("Duplicated dependency declaration: {s}. Skipping", .{identifier});
+            continue;
+        }
+
+        value.value_ptr.* = dep;
+    }
+
+    var ix: usize = 0;
+    while (true) {
+        const values = dependencies.values();
+        if (values.len <= ix) break;
+        const dep = values[ix];
+
+        const baseUrl = dep.uri(allocator, spec.defaultMaven) catch |err| {
+            std.log.err("Failed to resolve url: {any}", .{err});
+            failure = true;
             continue;
         };
 
-        defer allocator.free(identifier);
-        if (!depsset.contains(identifier)) {
-            const baseUrl = dep.uri(allocator, spec.defaultMaven) catch |err| {
-                std.log.err("Failed to resolve url: {any}", .{err});
-                failure = true;
-                continue;
-            };
+        defer allocator.free(baseUrl);
+        const pom = dep.remoteFilename(allocator, .pom) catch |err| {
+            std.log.err("Failed to resolve remote filename: {any}", .{err});
+            failure = true;
+            continue;
+        };
 
-            defer allocator.free(baseUrl);
-            const pom = dep.remoteFilename(allocator, .pom) catch |err| {
-                std.log.err("Failed to resolve remote filename: {any}", .{err});
-                failure = true;
-                continue;
-            };
+        const parts = [_][]const u8{ baseUrl, pom };
 
-            const parts = [_][]const u8{ baseUrl, pom };
+        const url = try std.mem.joinZ(allocator, "/", &parts);
 
-            const url = std.mem.joinZ(allocator, "/", &parts) catch |err| {
-                std.log.err("Failed to resolve full url: {any}", .{err});
-                failure = true;
-                continue;
-            };
+        const reader = try downloadManager.download(url);
+        defer reader.deinit();
 
-            const reader = downloadManager.download(url) catch |err| {
-                std.log.err("Unable to download pom: {any}", .{err});
-                failure = true;
-                continue;
-            };
-            defer reader.deinit();
+        var xml = std.ArrayList(u8).init(allocator);
+        defer xml.deinit();
+        try xml.appendSlice(reader.asSlice());
+        var buffered = std.io.fixedBufferStream(xml.items);
+        const bufferedReader = buffered.reader();
+        var xmlReader = try pomParser.parseDeps(allocator, bufferedReader);
 
-            var xml = std.ArrayList(u8).init(allocator);
-            defer xml.deinit();
-            xml.appendSlice(reader.asSlice()) catch |err| {
-                std.log.err("Unable to acquire pom: {any}", .{err});
-                failure = true;
-                continue;
-            };
+        while (xmlReader.next(allocator)) |asset| {
+            const identifier = try asset.identifier(allocator);
+            defer allocator.free(identifier);
 
-            var buffered = std.io.fixedBufferStream(xml.items);
-            const bufferedReader = buffered.reader();
-            var xmlReader = try pomParser.parseDeps(allocator, bufferedReader);
-
-            while (xmlReader.next(allocator)) |asset| {
-                std.log.debug("Has dependency: {s}:{s}:{s}", .{ asset.group, asset.artifact, asset.version });
-                asset.deinit();
-            }
-
-            const exists = localrepo.exists(allocator, dep, .jar) catch |err| {
-                std.log.err("Failed to verify if jar exists, skipping: {any}", .{err});
-                continue;
-            };
-            if (!exists) {
-                depsset.insert(identifier) catch |err| {
-                    std.log.warn(
-                        "Unable to insert dependency identifier into cache, might collide: {any}",
-                        .{err},
-                    );
-                };
-                const item = try allocator.create(worker.WorkItem);
-                item.* = worker.WorkItem.Dep(dep);
-                pool.enqueue(item) catch |err| {
-                    std.log.err("Unable to enqueue: {any}", .{err});
-                    failure = true;
-                };
+            const next = try dependencies.getOrPut(identifier);
+            if (next.found_existing) {
+                if (!std.mem.eql(u8, next.value_ptr.version, asset.version)) {
+                    std.log.warn("Asset {s} already included at a different version {s} != {s}", .{
+                        identifier,
+                        next.value_ptr.version,
+                        asset.version,
+                    });
+                } else {
+                    continue;
+                }
             } else {
-                std.log.debug("Dependency {s} already present", .{identifier});
-                depsset.insert(identifier) catch |err| {
-                    std.log.warn(
-                        "Unable to insert dependency identifier into cache, might collide: {any}",
-                        .{err},
-                    );
-                };
+                next.value_ptr.* = asset;
             }
-        } else {
-            std.log.debug("Dependency {s} already processed", .{identifier});
         }
+        ix += 1;
+    }
+
+    for (dependencies.values()) |dep| {
+        const exists = localrepo.exists(allocator, dep, .jar) catch |err| {
+            std.log.err("Failed to verify if jar exists, skipping: {any}", .{err});
+            failure = true;
+            continue;
+        };
+        if (!exists) {
+            const item = try allocator.create(worker.WorkItem);
+            item.* = worker.WorkItem.Dep(dep);
+            pool.enqueue(item) catch |err| {
+                std.log.err("Unable to enqueue: {any}", .{err});
+                failure = true;
+            };
+        }
+
+        // var target = json.writeStream(std.io.getStdOut().writer(), .{ .whitespace = .minified });
+        // try target.beginObject();
+        // try target.objectField("dependencies");
+        // try target.beginArray();
     }
 }
