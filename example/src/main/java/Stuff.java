125d3fb623d12e4ff635cacdacdba0d949e56ba2
Henry Kupty <hkupty@gmail.com>
2025-08-14 12:51:11 +0200

    docs(example): Improve example app

--- example/src/main/java/Stuff.java
+++ example/src/main/java/Stuff.java
@@ -1,7 +1,12 @@
 package stuff;
 
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
 public class Stuff {
+  static Logger logger = LoggerFactory.getLogger(Stuff.class);
   public static void main(String[] args) {
+    logger.info("Hello lift from Penna");
     System.out.println("Hello Lift!");
   }
 }
